981f821136b3da413e66086c8c39881b1467d614
Bruno <dollarperuano@gmail.com>
2026-03-23 23:28:05 -0500

    site off

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-const ENABLE_SITE = true
+const ENABLE_SITE = false
 
 export function middleware(request: NextRequest) {
   if (!ENABLE_SITE) {
